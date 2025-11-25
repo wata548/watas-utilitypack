cd623f8e21b893035d27f062de949a09989e3bce
wata548 <watashimokia@gmail.com>
2025-11-25 19:08:19 +0900

    csv table's add function

--- Runtime/Lang/Table/CSVPack.cs
+++ Runtime/Lang/Table/CSVPack.cs
@@ -8,28 +8,32 @@ namespace Lang {
     public class CSVPack: ILangTable {
 
         //==================================================||Fields 
-        private readonly IReadOnlyDictionary<string, string[]> _table;
-        private readonly IEnumerable<Language> _allowLanguages;
+        private readonly Dictionary<string, string[]> _table = new();
+        private List<Language> _allowLanguages = new();
         
         //==================================================||Constructors 
         public CSVPack(string pPath): this(File.ReadAllText(pPath), LanguageManager.MainLang) { }
         public CSVPack(string pContext, Language pMainLanguage): this(CSV.Parse(pContext), pMainLanguage) {}
-        public CSVPack(List<List<string>> pContext, Language pMainLanguage) {
+        public CSVPack(List<List<string>> pContext, Language pMainLanguage) =>
+            Add(pContext, pMainLanguage);
+
+        public IEnumerable<Language> AllowLanguages() =>
+            _allowLanguages;
+
+        public void Add(List<List<string>> pContext, Language pMainLanguage) {
             var packs = pContext.Select(row => ((Language)Enum.Parse(typeof(Language), row[0]), row.Skip(1)));
             if (packs.All(pack => pack.Item1 != pMainLanguage))
                 return;
 
             var length = Enum.GetValues(typeof(Language)).Length;
-            var table = new Dictionary<string, string[]>();
             var mainPack = packs.First(pack => pack.Item1 == pMainLanguage).Item2.ToList();
             foreach (var word in mainPack) {
-                table.Add(word, new string[length]);
-                table[word][(int)pMainLanguage] = word;
+                _table.TryAdd(word, new string[length]);
+                _table[word][(int)pMainLanguage] = word;
             }
 
-            var allowLanguages = new List<Language>();
             foreach (var pack in packs) {
-                allowLanguages.Add(pack.Item1);
+                _allowLanguages.Add(pack.Item1);
                 
                 if(pack.Item1 == pMainLanguage)
                     continue;
@@ -37,18 +41,12 @@ namespace Lang {
                 var idx = 0;
                 foreach (var word in pack.Item2) {
                     var key = mainPack[idx];
-                    table[key][(int)pack.Item1] = word;
+                    _table[key][(int)pack.Item1] = word;
                     idx++;
                 }
-                
             }
-
-            _table = table;
-            _allowLanguages = allowLanguages;
+            _allowLanguages = _allowLanguages.Distinct().OrderBy(lang => lang).ToList();
         }
-
-        public IEnumerable<Language> AllowLanguages() =>
-            _allowLanguages;
         //==================================================||Methods 
         public string Text(Language pLang, string pContext) {
             if(_table.TryGetValue(pContext, out string[] value))

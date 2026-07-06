537148765cec70f4a0a4f4143d622b068ddbb5cb
임지우 <watashimokia@gmail.com>
2026-07-06 20:10:34 +0900

    add smartload: List<List<string>>

--- Runtime/CSV/Extensions/SmartLoad.cs
+++ Runtime/CSV/Extensions/SmartLoad.cs
@@ -12,6 +12,16 @@ namespace CSVData.Extensions {
         /// <param name="pSheet">FileName / Spread sheet's sheet name</param>
         /// <param name="pDirectory">directory path(Resources/{pDirectory})</param>
         public static List<T> SmartLoad<T>(string pSheet, string pDirectory = "") {
+            return CSV.DeserializeToList<T>(SmartLoad(pSheet, pDirectory);
+        }
+        /// <summary>
+        /// <p> It depends on csv window's parameter(in editor not runtime)</p>
+        /// <p> This function is only workable when CSV save path in Resource directory </p>
+        /// <p> CSV window's 'save data path' </p>
+        /// </summary>
+        /// <param name="pSheet">FileName / Spread sheet's sheet name</param>
+        /// <param name="pDirectory">directory path(Resources/{pDirectory})</param>
+        public static List<List<string>> SmartLoad(string pSheet, string pDirectory = "") {
             bool isAutoSave = false;
             var context = new List<List<string>>();
 #if UNITY_EDITOR
@@ -30,8 +40,9 @@ namespace CSVData.Extensions {
                 var resource = Resources.Load<TextAsset>(Path.Join(pDirectory, pSheet + ".csv"));
                 context = CSV.Parse(resource.text);    
             }
-            return CSV.DeserializeToList<T>(context);
+            return context;
 
         }
+    }
     }
 }
